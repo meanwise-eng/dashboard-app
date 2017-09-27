558e868a481a1479461a463f0fbbaa5656efc5c1
PunitGr <groove679@gmail.com>
2017-09-28 02:48:00 +0530

    Add configuration files for tslint and tsconfig + change file extensions

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+// @flow
+import React, { Component } from "react";
+import Login from "./Login";
+
+export default class App extends Component<{}> {
+    render() {
+        return (
+            <div>
+                <Login />
+            </div>
+        );
+    }
+}
